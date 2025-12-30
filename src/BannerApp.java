f9b53be575fa65e82cef9f314a638b79bb20e2fd
BL-Vinoth <vinoth.p@bridgelabz.com>
2025-12-30 16:22:05 +0530

    BannerApp

---
+++ src/BannerApp.java
@@ -0,0 +1,10 @@
+public class BannerApp {
+
+    public static void main(String[] args){
+        String data = "OOPS";
+
+        System.out.println(data);
+
+        BannerApp_User bannerApp = new BannerApp_User();
+    }
+}
