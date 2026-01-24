5dcf1b99ac33ca808e7685ec156f429eab914ed3
vinoth9598 <pvinoth9598@gmail.com>
2026-01-24 14:37:36 +0530

    day42 form validation

---
+++ script.js
@@ -0,0 +1,55 @@
+
+function validateDetails(){
+    let name = document.getElementById("text").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+    let phoneNumber = document.getElementById("phoneNumber").value.trime();
+
+    let isValid = true ;
+
+    // patterns
+    let namePattern = /^[A-Za-z ]{3,}$/;
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+    let passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/ ;
+    let phonePattern = /^[6-9]\d{9}$/; 
+
+    // Name validation
+    if(!namePattern.test(name)){
+        document.getElementById("nameError").innerText =
+       "Name must contain only letters and minimum 3 characters";
+       isValid = false ;
+    }
+
+    // Email validation
+    if(!emailPattern.test(email)){
+        document.getElementById("emailError").innerText = 
+        "Please enter a valid Gmail address";
+
+        isValid = false ;
+    }
+
+    // password validation
+    if(!passwordPattern.test(password)){
+        document.getElementById("passwordError").innerText = 
+        "Password must be at least 8 characters with letters and numbers";
+
+        isValid = false ;
+    }
+
+    // phone number validation
+    if(phonePattern.test(phoneNumber)){
+        document.getElementById("phoneError").innerText = 
+        "phone number must be 10 characters and numbers only";
+
+        isValid = false
+    }
+
+    // clear previous error
+    document.getElementById("nameError").innerHTML = "";
+    document.getElementById("emailError").innerHTML = "";
+    document.getElementById("passwordError").innerHTML = "";
+    document.getElementById("phoneError").innerHTML = "";
+
+    return isValid ;
+
+}
